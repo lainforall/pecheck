62dd05b3c3f91d1cc49ea89ad1f5c1d25fb9e5bd
Ivan <adolfcitler1488@outlook.com>
2018-07-31 21:13:36 +0300

    Add project files.

---
+++ PEheader/PEheader.cpp
@@ -0,0 +1,21 @@
+// PEheader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdint.h"
+
+typedef uint8_t  BYTE;
+typedef uint32_t DWORD;
+typedef int32_t  LONG;
+typedef uint16_t WORD;
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
+
